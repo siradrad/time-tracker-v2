52b593a274c18e79e51e7c60709a3c4b2de20bed
Christopher D Dardaris <christopherddardaris@Mac.localdomain>
2025-05-25 11:40:17 -0400

    Final configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/time-tracker-v2/',
+  base: '/',
   server: {
     host: true,
     port: 3000,
