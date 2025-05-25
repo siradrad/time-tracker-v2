6f59547874681b7c7ca66c7e3ba20bd57d5ec5a1
Christopher D Dardaris <christopherddardaris@Mac.localdomain>
2025-05-25 11:27:56 -0400

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/time-tracker-v2/',
   server: {
     host: true,
     port: 3000,
